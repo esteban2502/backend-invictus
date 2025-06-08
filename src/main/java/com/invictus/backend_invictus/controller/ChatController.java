9449f261284f47e06c33657cb2ba1967309df983
esteban2502 <juanes9001@gmail.com>
2025-06-07 20:18:59 -0500

    creacion del proyecto

---
+++ src/main/java/com/invictus/backend_invictus/controller/ChatController.java
@@ -0,0 +1,8 @@
+package com.invictus.backend_invictus.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+}
---
+++ src/main/java/com/invictus/backend_invictus/service/ChatService.java
@@ -0,0 +1,14 @@
+package com.invictus.backend_invictus.service;
+
+
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+
+
+
+}
