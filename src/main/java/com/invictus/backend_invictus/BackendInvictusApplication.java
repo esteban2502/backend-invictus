3a6f21f519602a90b6ef6e7d75acf4e00846b1c6
esteban2502 <juanes9001@gmail.com>
2025-06-07 19:45:52 -0500

    creacion del proyecto

---
+++ src/main/java/com/invictus/backend_invictus/BackendInvictusApplication.java
@@ -0,0 +1,13 @@
+package com.invictus.backend_invictus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInvictusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInvictusApplication.class, args);
+	}
+
+}
